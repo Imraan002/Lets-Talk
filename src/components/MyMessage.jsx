c7ed42f6eac06c3c7bd0b949f7a409eb0f7eccc7
Imraan <imran2hosain@gmail.com>
2022-01-23 15:30:28 +0530

    2nd commit

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,19 +1,31 @@
+
+
+
 const MyMessage = ({ message }) => {
     if (message.attachments && message.attachments.length > 0) {
       return (
+        <>
+        <div style={{float: 'right', marginRight: '18px', color: 'white'}}>
+        <div style={{marginLeft:'100px',fontSize: 10,color: 'black'   }}>
+        {message.sender.username}</div>
         <img
           src={message.attachments[0].file}
           alt="message-attachment"
           className="message-image"
           style={{ float: 'right' }}
         />
+        </div>
+        </>
       );
     }
   
     return (
-      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#66c7d4' }}>
-        {message.text}
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: "#065f9e", fontSize: 18 }}>
+        <div style={{fontSize: 10,color: 'orange'  }}>
+        {message.sender.username}</div>
+        {message.text}   
       </div>
+    
     );
   };
   
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,31 +1,49 @@
 const TheirMessage = ({ lastMessage, message }) => {
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
-  
-    return (
-      <div className="message-row">
-        {isFirstMessageByUser && (
-          <div
-            className="message-avatar"
-            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
-          />
-        )}
-        {message.attachments && message.attachments.length > 0
-          ? (
-            <img
-              src={message.attachments[0].file}
-              alt="message-attachment"
-              className="message-image"
-              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
-            />
-          )
-          : (
-            <div className="message" style={{ float: 'left', color:'white',backgroundColor: '#392fcc', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
-              {message.text}
-            </div>
-          )}
-      </div>
-    );
-  };
-  
-  export default TheirMessage;
-  
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
+      )}
+      {message.attachments && message.attachments.length > 0 ? (
+       <div>
+         <div style={{fontSize:10,color:'blue',marginLeft:'150px'}}>
+          {message.sender.username}
+        </div>
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+        </div>
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            color: "white",
+            backgroundColor: "#6e1973",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+            fontSize: 18
+          }}
+        > 
+        <div style={{fontSize:10,color:'yellow'}}>
+          {message.sender.username}
+        </div>
+          {message.text}
+        </div>
+      )}
+    </div>
+    
+  );
+};
+
+export default TheirMessage;
